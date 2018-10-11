b7f58e8965f264396b187a837e3319b01076d637
Cepilloman <farango@ideassimples.com.co>
2018-10-11 13:41:31 -0500

    lifecycle components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,
+  OnInit, OnChanges, DoCheck,OnDestroy,
+  AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
     <app-ng-style></app-ng-style>
+
     <app-css></app-css>
+
     <app-clases></app-clases>
+
     <p [appResaltado]="'gray'" > Hola mundo </p>
 
     <app-ng-switch></app-ng-switch>
-  `,
-  styles: []
+  `
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck,OnDestroy,
+AfterContentInit, AfterContentChecked,
+AfterViewInit, AfterViewChecked  {
 
-  constructor() { }
+  constructor() {
+    console.log("Constructor - 1");
+  }
 
   ngOnInit() {
+    console.log("OnInit - 2");
+  }
+
+  ngOnChanges(){
+    console.log("OnChanges - 3");
+  }
+
+  ngDoCheck(){
+    console.log("DoCheck - 4");
+  }
+
+  ngOnDestroy(){
+    console.log("OnDestroy - 5");
+  }
+
+  ngAfterContentInit(){
+    console.log("AfterContentInit - 6");
+  }
+
+  ngAfterContentChecked(){
+    console.log("AfterContentChecked - 7");
+  }
+
+  ngAfterViewInit(){
+    console.log("AfterViewInit - 8");
+  }
+
+  ngAfterViewChecked(){
+    console.log("AfterViewChecked - 9");
   }
 
 }
